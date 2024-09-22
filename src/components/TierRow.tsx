9b71acccb1f819a8fd7ff3075725bac779f900f8
Lou TIGROUDJA <lou.tigroudja07@gmail.com>
2024-09-22 19:21:43 +0200

    Edit bin icon

--- src/components/TierRow.tsx
+++ src/components/TierRow.tsx
@@ -77,7 +77,10 @@ export const TierRow: FC<TierRowProps> = ({
             className="w-full text-center bg-transparent border-none focus:outline-none dark:text-gray-800"
           />
         ) : (
-          <span onClick={handleNameClick} className="cursor-pointer dark:text-gray-800">
+          <span
+            onClick={handleNameClick}
+            className="cursor-pointer dark:text-gray-800"
+          >
             {tier.name}
           </span>
         )}
@@ -102,18 +105,22 @@ export const TierRow: FC<TierRowProps> = ({
           className="text-red-500 hover:text-red-700 dark:text-red-400 dark:hover:text-red-600 transition-colors duration-200"
         >
           <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
+              d="M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z"
+              fill="currentColor"
+              fillRule="evenodd"
+              clipRule="evenodd"
               strokeLinecap="round"
               strokeLinejoin="round"
-              strokeWidth={2}
-              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-            />
+            ></path>
           </svg>
         </button>
       </div>
